dad76110436c47ba47b3b7592bce4806717f18f2
1275949954@qq.com <1653317542@qq.com>
2023-07-15 23:27:42 +0800

    hot-fix commit 2

--- src/main/java/cn/itzj/test.java
+++ src/main/java/cn/itzj/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("11111111111");
         System.out.println("22222222222");
+        System.out.println("hot fix 33333");
     }
 }
